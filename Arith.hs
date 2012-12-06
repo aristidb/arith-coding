06ef52bee2147e186cba0c13d8dc482fed9c6b51
Aristid Breitkreuz <aristidb@gmail.com>
2012-12-06 23:51:48 +0100

    use new Model in Arith

--- Arith.hs
+++ Arith.hs
@@ -1,20 +1,21 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, RankNTypes #-}
 module Arith where
 
-import           Data.List
-import           Interval
-import           Model
-import           Prob (Prob)
+import Control.Lens
+import Data.List
+import Interval
+import Model
+import Prob (Prob)
 
 encodeStep :: Model a -> Sym a -> PInterval -> PInterval
-encodeStep m x outer = embed outer (enc m x)
+encodeStep m x outer = embed outer (review m x)
 
 encode :: Model a -> [a] -> Prob
-encode model xs = midpoint $ foldl' (flip $ encodeStep model) initial (map Sym xs ++ [EOF])
+encode m xs = midpoint $ foldl' (flip $ encodeStep m) initial (map Sym xs ++ [EOF])
 
 decode :: Model a -> Prob -> [a]
-decode model p = go (PI p p)
-  where go inner = case dec model inner of
-                     Decode w@(Sym c) -> c : go (unembed (enc model w) inner)
-                     Decode EOF -> []
-                     NoDecode -> error "Invalid stream"
+decode m p = go (PI p p)
+  where go inner = case inner ^? m of
+                     Just w@(Sym c) -> c : go (unembed (review m w) inner)
+                     Just EOF -> []
+                     Nothing -> error "Invalid stream"
