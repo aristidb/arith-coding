65fbb28a48805229d54dd9d4a85e45122ee06d62
Aristid Breitkreuz <aristidb@gmail.com>
2012-12-06 01:53:01 +0100

    lensify Model (eofModel does not work yet due to https://github.com/ekmett/lens/issues/176)

--- Model.hs
+++ Model.hs
@@ -1,18 +1,20 @@
-{-# LANGUAGE ScopedTypeVariables, DeriveFunctor #-}
+{-# LANGUAGE ScopedTypeVariables, DeriveFunctor, TypeFamilies, RankNTypes #-}
 module Model where
 
-import Interval
-import Prob (Prob(..))
-import Data.Ratio
 import Control.Applicative
+import Control.Category
 import Control.Lens
+import Data.Ratio
+import Interval
+import Prelude hiding (id, (.))
+import Prob (Prob(..))
 --import Test.QuickCheck
 
 data Sym a = Sym a | EOF
   deriving (Show, Eq, Functor)
 
 sym :: Iso (Maybe a) (Maybe b) (Sym a) (Sym b)
-sym = isos f g f g
+sym = iso f g
   where
     f (Just a) = Sym a
     f Nothing = EOF
@@ -20,24 +22,14 @@ sym = isos f g f g
     g (Sym a) = Just a
     g EOF = Nothing
 
-data Decode a = Decode a | NoDecode
-  deriving (Show, Eq, Functor)
-
-decodeSym :: Simple Iso (Decode (Maybe a)) (Decode (Sym a))
-decodeSym = mapping sym
-
-data PureModel a = Model {
-    enc :: a -> PInterval
-  , dec :: PInterval -> Decode a
-  }
-
+type PureModel a = Simple Prism PInterval a
 type Model a = PureModel (Sym a)
 
 prop_ModelRoundtrip :: Eq a => PureModel a -> a -> Bool
-prop_ModelRoundtrip m a = dec m (enc m a) == Decode a
+prop_ModelRoundtrip m x = review m x ^? m == Just x
 
 stdEnumModel :: forall a. (Bounded a, Enum a) => PureModel a
-stdEnumModel = Model { enc = enco, dec = deco }
+stdEnumModel = prism enco deco
   where
     mini, maxi :: a
     mini = minBound
@@ -50,25 +42,24 @@ stdEnumModel = Model { enc = enco, dec = deco }
 
     deco rng@(PI (Prob a) _)
       = if rng `isSubintervalOf` enco (x :: a)
-        then Decode x
-        else NoDecode
+        then Right x
+        else Left rng
       where x = toEnum (min i (fromEnum maxi))
             i = floor (a * fromInteger num)
 
 maybeModel :: forall a. Prob -> PureModel a -> PureModel (Maybe a)
-maybeModel p m = Model { enc = enco, dec = deco }
+maybeModel p m = prism enco deco
   where
     r1 = PI 0 p
     r2 = PI p 1
     
     enco Nothing = r1
-    enco (Just x) = embed r2 (enc m x)
+    enco (Just x) = embed r2 (review m x)
 
-    deco rng | rng `isSubintervalOf` r1 = Decode Nothing
-             | rng `isSubintervalOf` r2 = Just <$> dec m (unembed r2 rng)
-             | otherwise = NoDecode
+    deco :: PInterval -> Either PInterval (Maybe a)
+    deco rng | rng `isSubintervalOf` r1 = Right Nothing
+             | rng `isSubintervalOf` r2 = Right (unembed r2 rng ^? m)
+             | otherwise = Left rng
 
-eofModel :: forall a. Prob -> PureModel a -> Model a
-eofModel p m = Model { enc = enc mMod . review sym
-                     , dec = view decodeSym . dec mMod }
-  where mMod = maybeModel p m
+--eofModel :: forall a. Prob -> PureModel a -> PureModel (Sym a)
+--eofModel p m = maybeModel p m . sym
