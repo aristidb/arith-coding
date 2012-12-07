2ce1cb5167c2eed29549105ae66fb453ce7b03ad
Aristid Breitkreuz <aristidb@gmail.com>
2012-12-07 22:19:34 +0100

    make decode total

--- Arith.hs
+++ Arith.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving, RankNTypes #-}
 module Arith where
 
+import Control.Applicative
 import Control.Category
 import Control.Lens
 import Data.List
@@ -8,6 +9,8 @@ import Interval
 import Model
 import Prelude hiding (id, (.))
 import Prob (Prob)
+import Test.QuickCheck
+import Test.QuickCheck.Property
 
 encodeStep :: Model a -> Sym a -> PInterval -> PInterval
 encodeStep m x inner = (inner, x) ^. remit m
@@ -15,14 +18,20 @@ encodeStep m x inner = (inner, x) ^. remit m
 encode :: Model a -> [a] -> PInterval
 encode m xs = foldr (encodeStep m) initial (map Sym xs ++ [EOF])
 
-decode :: Model a -> PInterval -> [a]
+decode :: Model a -> PInterval -> Maybe [a]
 decode m = go
   where go inner = case inner ^? m of
-                     Just (r, w@(Sym c)) -> c : go r
-                     Just (_, EOF) -> []
-                     Nothing -> error "Invalid stream"
+                     Just (r, w@(Sym c)) -> (c :) <$> go r
+                     Just (_, EOF) -> Just []
+                     Nothing -> Nothing
 
 prop_roundtripA :: Eq a => Model a -> [a] -> Bool
-prop_roundtripA m xs = decode m (encode m xs) == xs
+prop_roundtripA m xs = decode m (encode m xs) == Just xs
 
---prop_roundtripB :: Eq a => Model a -> PInterval -> Bool
+prop_roundtripB :: Eq a => Model a -> PInterval -> Property
+prop_roundtripB m r = case decode m r of
+                        Nothing -> property rejected
+                        Just xs -> property $ r `isSubintervalOf` encode m xs
+
+prop_roundtrip :: (Eq a, Show a, Arbitrary a) => Model a -> Property
+prop_roundtrip m = prop_roundtripA m .&. prop_roundtripB m
